e3ce93f91d616e8bca3f019d7b879fd5387fe1b9
Andy Park <atpark333@gmail.com>
2020-02-13 19:39:43 +0900

    Update deps

--- apps/rumbl/mix.exs
+++ apps/rumbl/mix.exs
@@ -36,8 +36,8 @@ defmodule Rumbl.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
+      {:ecto_sql, "~> 3.3"},
+      {:postgrex, "~> 0.15.3"},
       {:jason, "~> 1.0"},
       {:pbkdf2_elixir, "~> 1.1"}
     ]
